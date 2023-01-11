c06c4e4d16f9ba2f55a66ddcde768dd6c654d782
XJX <2768189171@qq.com>
2023-01-11 15:31:42 +0800

    gitee pull

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("push test!!!");
         System.out.println("pull test!!!");
         System.out.println("pull test222!!!");
+        System.out.println("pull gitee!!!");
     }
 }
