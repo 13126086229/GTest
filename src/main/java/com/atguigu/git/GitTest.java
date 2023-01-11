6ec1f9061fcd2b92b879686c101b3c8cad95614b
XJX <2768189171@qq.com>
2023-01-11 12:38:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 谢继祥
+ * @version 1.0
+ * @date 2023/1/11 12:34
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!!!");
+    }
+}
